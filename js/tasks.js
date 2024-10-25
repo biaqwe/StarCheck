23b19f9c25661429d25c1c0af34d16c6e35ddeaf
Bianca Balaș <biancabalas2907@yahoo.com>
2024-10-25 22:10:28 +0300

    Changes to save theme preference

--- js/tasks.js
+++ js/tasks.js
@@ -248,22 +248,39 @@ logoutBtn.addEventListener('click', ()=>{
 });
 
 document.addEventListener('DOMContentLoaded', ()=>{
+    //save theme preference in local storage to keep after page refresh and across pages
+    const body=document.body;
+    const theme=document.getElementById('theme');
+    const themeIcon=theme.querySelector("i");
+    const storedTheme=localStorage.getItem('theme');
+
+    if(storedTheme==='dark'){
+        body.classList.add('dark');
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    }
+    else{
+        body.classList.remove('dark');
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+
     fetchTasks();
     fetchName();
 
     //dark mode toggle
-    const theme=document.getElementById('theme');
-    const themeIcon=theme.querySelector("i");
-    const body=document.body;
     theme.addEventListener('click', function(){
         body.classList.toggle('dark');
         if(body.classList.contains('dark')){
             themeIcon.classList.remove('fa-moon');
             themeIcon.classList.add('fa-sun');
+            localStorage.setItem('theme', 'dark');
         }
         else{
             themeIcon.classList.remove('fa-sun');
             themeIcon.classList.add('fa-moon');
+            localStorage.setItem('theme', 'light');
         }
     });
 });
+
