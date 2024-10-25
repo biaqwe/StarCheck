d09a4c2c8168cf9b2862a4976e64e644efd46983
Bianca Balaș <biancabalas2907@yahoo.com>
2024-10-25 21:54:46 +0300

    Changes for dark mode

--- js/login.js
+++ js/login.js
@@ -8,4 +8,20 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         document.getElementById('errorMsg').innerText=message;
     }
+
+    //dark mode toggle
+    const theme=document.getElementById('theme');
+    const themeIcon=document.querySelector("i");
+    const body=document.body;
+    theme.addEventListener('click', function(){
+        body.classList.toggle('dark');
+        if(body.classList.contains('dark')){
+            themeIcon.classList.remove('fa-moon');
+            themeIcon.classList.add('fa-sun');
+        }
+        else{
+            themeIcon.classList.remove('fa-sun');
+            themeIcon.classList.add('fa-moon');
+        }
+    });
 });
